79903e3e160c818d5753087878fb8f6c9ac65e61
RaulDiaz <rulhdiaz@gmail.com>
2023-04-05 11:56:45 -0600

    primer commit raul

--- arturito/src/components/PlanetsSection/index.tsx
+++ arturito/src/components/PlanetsSection/index.tsx
@@ -40,10 +40,10 @@ const Planets = () => {
   if (!data) {
     return <div className="px-2">Loading...</div>;
   }
-
+//cambio
   return (
     <div>
-      <Table columns={columns} data={data.results} /* :D */ />
+      <Table columns={columns} data={data.results} /* :D f */ />
     </div>
   );
 };
